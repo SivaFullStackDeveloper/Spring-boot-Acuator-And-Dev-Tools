109acb8afba890bd51b78e44faca5e2c602d8874
unknown <sivaflutterdeveloper@gmail.com>
2024-11-28 18:29:34 +0530

    intial commit with dependencies

---
+++ src/main/java/com/siva/AcuatorAndDevTools/AcuatorAndDevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.siva.AcuatorAndDevTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcuatorAndDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcuatorAndDevToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siva/AcuatorAndDevTools/AcuatorAndDevToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siva.AcuatorAndDevTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcuatorAndDevToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
